35a70f06d3e879adfe5b992a4151d628d2ebdc5f
Daniel Price <daniel.price@monash.edu>
2019-03-08 15:36:05 +1100

    (opt2slack) small script to convert opt html files into slack message

---
+++ opt2slack.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+#
+# script to parse .html files from performance results
+# to create slack message
+#
+# Daniel Price, Mar 2019
+#
+if ($#ARGV < 0) {
+   die "Usage: $0 opt-status-gfortran.html\n";
+}
+foreach my $file (@ARGV) {
+   open my $fh, '<', $file or die "can't open $file\n";
+   while (<$fh>) {
+      #print "$_\n";
+      my $line="$_";
+      if ($line =~ m/SYSTEM=(\w+)\W/) {
+         print "SYSTEM=$1\\n";
+      }
+      if ($line =~ m/\<td bgcolor=\".*\"\>(\w+)\<\/td\>\<td\>(.*)\<\/td\>\<td.*\>(.*)\<\/td\>\<td\>(.*)\<\/td\>\<\/tr\>/) {
+         print "$1: ${2}s change $3% error $4\\n";
+      }
+   }
+}
